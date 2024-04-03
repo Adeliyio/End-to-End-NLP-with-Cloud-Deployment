2cc76aaf1abcfe8d94d6c14a7e9aae7bdafe9bb3
Adeliyio <adeliyitomiwa@yahoo.com>
2024-04-03 13:57:52 +0100

    Requirements Added and Setup Done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="NLP Classification",
+    version="0.0.1",
+    author="Adeliyio",
+    author_email="oadatascientist@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
